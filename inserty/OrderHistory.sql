9f1ce69a6293c2a4f3e07dd15567cdc508f3c786
folusz-sebastian <folusz.sebastian@gmail.com>
2020-06-05 19:19:54 +0200

    changed Order and added OrderHistory

---
+++ inserty/OrderHistory.sql
@@ -0,0 +1,10 @@
+insert into [Order] (albumId, clientId, orderDate, sysStartTime, sysEndTime) values (20, 6 , '2019-10-26 05:03:45', '2019-10-26 05:03:45', '9999-12-31 23:59:59.9999999');
+insert into [Order] (albumId, clientId, orderDate, sysStartTime, sysEndTime) values (21, 27, '2019-06-15 00:26:23', '2019-06-15 00:26:23', '9999-12-31 23:59:59.9999999');
+insert into [Order] (albumId, clientId, orderDate, sysStartTime, sysEndTime) values (20, 26, '2020-05-08 19:56:49', '2020-05-08 19:56:49', '9999-12-31 23:59:59.9999999');
+insert into [Order] (albumId, clientId, orderDate, sysStartTime, sysEndTime) values (14, 16, '2019-02-20 07:42:56', '2019-02-20 07:42:56', '9999-12-31 23:59:59.9999999');
+insert into [Order] (albumId, clientId, orderDate, sysStartTime, sysEndTime) values (2 , 3 , '2018-06-14 10:58:31', '2018-06-14 10:58:31', '9999-12-31 23:59:59.9999999');
+insert into [Order] (albumId, clientId, orderDate, sysStartTime, sysEndTime) values (10, 2 , '2020-01-10 11:53:12', '2020-01-10 11:53:12', '9999-12-31 23:59:59.9999999');
+insert into [Order] (albumId, clientId, orderDate, sysStartTime, sysEndTime) values (25, 2 , '2018-11-06 00:08:21', '2018-11-06 00:08:21', '9999-12-31 23:59:59.9999999');
+insert into [Order] (albumId, clientId, orderDate, sysStartTime, sysEndTime) values (12, 30, '2018-07-16 23:50:56', '2018-07-16 23:50:56', '9999-12-31 23:59:59.9999999');
+insert into [Order] (albumId, clientId, orderDate, sysStartTime, sysEndTime) values (2 , 14, '2019-08-19 21:41:41', '2019-08-19 21:41:41', '9999-12-31 23:59:59.9999999');
+insert into [Order] (albumId, clientId, orderDate, sysStartTime, sysEndTime) values (20, 24, '2019-06-15 19:58:07', '2019-06-15 19:58:07', '9999-12-31 23:59:59.9999999');
