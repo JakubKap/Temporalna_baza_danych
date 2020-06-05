3607d46dcab702bea9008c1e909423b761ffeff0
folusz-sebastian <folusz.sebastian@gmail.com>
2020-06-05 19:24:04 +0200

    changed ArtistBand and added ArtistBandHistory

---
+++ inserty/ArtistBandHistory.sql
@@ -0,0 +1,10 @@
+insert into ArtistBand (bandId, artistId, dateSinceArtistInBand, sysStartTime, sysEndTime) values (1, 5 , '1990-10-21 20:58:16', '1990-10-21 20:58:16', '9999-12-31 23:59:59.9999999');
+insert into ArtistBand (bandId, artistId, dateSinceArtistInBand, sysStartTime, sysEndTime) values (1, 11, '2005-06-12 08:56:50', '2005-06-12 08:56:50', '9999-12-31 23:59:59.9999999');
+insert into ArtistBand (bandId, artistId, dateSinceArtistInBand, sysStartTime, sysEndTime) values (1, 8 , '2011-12-01 16:10:19', '2011-12-01 16:10:19', '9999-12-31 23:59:59.9999999');
+insert into ArtistBand (bandId, artistId, dateSinceArtistInBand, sysStartTime, sysEndTime) values (1, 26, '1987-11-09 03:01:51', '1987-11-09 03:01:51', '9999-12-31 23:59:59.9999999');
+insert into ArtistBand (bandId, artistId, dateSinceArtistInBand, sysStartTime, sysEndTime) values (2, 7 , '1970-07-10 21:51:29', '1970-07-10 21:51:29', '9999-12-31 23:59:59.9999999');
+insert into ArtistBand (bandId, artistId, dateSinceArtistInBand, sysStartTime, sysEndTime) values (2, 1 , '2014-04-01 18:16:45', '2014-04-01 18:16:45', '9999-12-31 23:59:59.9999999');
+insert into ArtistBand (bandId, artistId, dateSinceArtistInBand, sysStartTime, sysEndTime) values (3, 4 , '2017-08-15 17:43:20', '2017-08-15 17:43:20', '9999-12-31 23:59:59.9999999');
+insert into ArtistBand (bandId, artistId, dateSinceArtistInBand, sysStartTime, sysEndTime) values (4, 13, '1985-01-06 21:18:14', '1985-01-06 21:18:14', '9999-12-31 23:59:59.9999999');
+insert into ArtistBand (bandId, artistId, dateSinceArtistInBand, sysStartTime, sysEndTime) values (4, 27, '2010-01-22 23:55:38', '2010-01-22 23:55:38', '9999-12-31 23:59:59.9999999');
+insert into ArtistBand (bandId, artistId, dateSinceArtistInBand, sysStartTime, sysEndTime) values (4, 20, '2010-06-08 09:19:01', '2010-06-08 09:19:01', '9999-12-31 23:59:59.9999999');
