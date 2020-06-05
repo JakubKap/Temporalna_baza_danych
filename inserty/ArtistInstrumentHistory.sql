3ba62071ea153ea14c2d7f5bfea800e9ec71ae0b
folusz-sebastian <folusz.sebastian@gmail.com>
2020-06-05 23:55:22 +0200

    Changed ArtistInstrument and added ArtistInstrumentHistory

---
+++ inserty/ArtistInstrumentHistory.sql
@@ -0,0 +1,29 @@
+
+# deleted
+insert into ArtistInstrumentHistory (artistId, instrumentName, dateSincePlaysInstrument, sysStartTime, sysEndTime) values (8 , 'Flet'         , '1985-11-12 22:42:21', '1985-11-12 22:42:21', '1995-04-04 02:06:12');
+insert into ArtistInstrumentHistory (artistId, instrumentName, dateSincePlaysInstrument, sysStartTime, sysEndTime) values (20, 'Flet'         , '2003-10-29 00:28:22', '2003-10-29 00:28:22', '2003-11-10 22:25:50');
+insert into ArtistInstrumentHistory (artistId, instrumentName, dateSincePlaysInstrument, sysStartTime, sysEndTime) values (4 , 'Gitara basowa', '1975-10-11 07:47:04', '1975-10-11 07:47:04', '1990-01-17 01:49:41');
+insert into ArtistInstrumentHistory (artistId, instrumentName, dateSincePlaysInstrument, sysStartTime, sysEndTime) values (9 , 'Gitara basowa', '1999-12-12 22:30:06', '1999-12-12 22:30:06', '2000-01-03 06:53:13');
+insert into ArtistInstrumentHistory (artistId, instrumentName, dateSincePlaysInstrument, sysStartTime, sysEndTime) values (7 , 'Pianino'      , '2006-05-19 06:35:58', '2006-05-19 06:35:58', '2009-05-19 07:38:59');
+
+# updated
+#2
+insert into ArtistInstrumentHistory (artistId, instrumentName, dateSincePlaysInstrument, sysStartTime, sysEndTime) values (10, 'Gitara elektryczna', '2003-03-01 10:55:08', '2003-03-01 10:55:08', '2003-03-10 18:46:49');
+insert into ArtistInstrumentHistory (artistId, instrumentName, dateSincePlaysInstrument, sysStartTime, sysEndTime) values (10, 'Gitara elektryczna', '2003-03-10 18:46:49', '2003-03-10 18:46:49', '2003-04-10 18:46:49');
+insert into ArtistInstrumentHistory (artistId, instrumentName, dateSincePlaysInstrument, sysStartTime, sysEndTime) values (10, 'Gitara elektryczna', '2003-04-10 18:46:49', '2003-04-10 18:46:49', '2006-04-10 18:46:49');
+#3
+insert into ArtistInstrumentHistory (artistId, instrumentName, dateSincePlaysInstrument, sysStartTime, sysEndTime) values (29, 'Gitara akustyczna' , '1979-02-17 23:48:28', '1979-02-17 23:48:28', '1979-02-20 23:41:50');
+insert into ArtistInstrumentHistory (artistId, instrumentName, dateSincePlaysInstrument, sysStartTime, sysEndTime) values (29, 'Gitara akustyczna' , '1979-02-20 23:41:50', '1979-02-20 23:41:50', '1979-02-22 07:09:12');
+insert into ArtistInstrumentHistory (artistId, instrumentName, dateSincePlaysInstrument, sysStartTime, sysEndTime) values (29, 'Gitara akustyczna' , '1979-02-22 07:09:12', '1979-02-22 07:09:12', '1979-03-15 06:05:34');
+insert into ArtistInstrumentHistory (artistId, instrumentName, dateSincePlaysInstrument, sysStartTime, sysEndTime) values (29, 'Gitara akustyczna' , '1979-03-15 06:05:34', '1979-03-15 06:05:34', '1979-03-17 05:09:44');
+#4
+insert into ArtistInstrumentHistory (artistId, instrumentName, dateSincePlaysInstrument, sysStartTime, sysEndTime) values (19, 'Gitara akustyczna' , '2000-06-24 00:20:11', '2000-06-24 00:20:11', '2002-02-24 16:03:29');
+insert into ArtistInstrumentHistory (artistId, instrumentName, dateSincePlaysInstrument, sysStartTime, sysEndTime) values (19, 'Gitara akustyczna' , '2002-02-24 16:03:29', '2002-02-24 16:03:29', '2002-06-24 00:20:11');
+#5
+insert into ArtistInstrumentHistory (artistId, instrumentName, dateSincePlaysInstrument, sysStartTime, sysEndTime) values (4 , 'Perkusja'          , '2013-06-03 00:14:31', '2013-06-03 00:14:31', '2013-08-06 04:16:55');
+#6
+insert into ArtistInstrumentHistory (artistId, instrumentName, dateSincePlaysInstrument, sysStartTime, sysEndTime) values (7 , 'Perkusja'          , '1985-01-08 14:54:00', '1985-01-08 14:54:00', '1987-01-08 23:22:21');
+#8
+insert into ArtistInstrumentHistory (artistId, instrumentName, dateSincePlaysInstrument, sysStartTime, sysEndTime) values (17, 'Keyboard'          , '1971-06-10 02:12:01', '1971-06-10 02:12:01', '1971-08-10 10:38:34');
+#9
+insert into ArtistInstrumentHistory (artistId, instrumentName, dateSincePlaysInstrument, sysStartTime, sysEndTime) values (16, 'Trąbka'            , '2013-03-08 12:36:26', '2013-03-08 12:36:26', '2016-01-12 23:24:44');
