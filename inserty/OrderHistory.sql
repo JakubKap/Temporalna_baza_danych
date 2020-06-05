50fb8fa13b5dd193fb11006a4cfa73111264abac
folusz-sebastian <folusz.sebastian@gmail.com>
2020-06-05 23:16:37 +0200

    Order -> OrderHistory in OrderHistory.sql

--- inserty/OrderHistory.sql
+++ inserty/OrderHistory.sql
@@ -1,26 +1,26 @@
 
 # deleted
-insert into [Order] (albumId, clientId, orderDate, sysStartTime, sysEndTime) values (29, 21, '2018-11-21 14:13:25', '2018-11-25 11:32:33', '2018-11-27 21:49:52');
-insert into [Order] (albumId, clientId, orderDate, sysStartTime, sysEndTime) values (22, 1 , '2020-01-09 08:52:33', '2020-03-09 04:04:19', '2020-03-15 13:42:35');
-insert into [Order] (albumId, clientId, orderDate, sysStartTime, sysEndTime) values (12, 22, '2019-03-09 14:33:16', '2019-05-12 16:20:17', '2019-07-08 11:12:51');
+insert into [OrderHistory] (albumId, clientId, orderDate, sysStartTime, sysEndTime) values (29, 21, '2018-11-21 14:13:25', '2018-11-25 11:32:33', '2018-11-27 21:49:52');
+insert into [OrderHistory] (albumId, clientId, orderDate, sysStartTime, sysEndTime) values (22, 1 , '2020-01-09 08:52:33', '2020-03-09 04:04:19', '2020-03-15 13:42:35');
+insert into [OrderHistory] (albumId, clientId, orderDate, sysStartTime, sysEndTime) values (12, 22, '2019-03-09 14:33:16', '2019-05-12 16:20:17', '2019-07-08 11:12:51');
 
 # updated
 #1
-insert into [Order] (albumId, clientId, orderDate, sysStartTime, sysEndTime) values (20, 6 , '2019-08-08 16:21:12', '2019-08-08 16:21:12', '2019-10-21 11:41:52');
-insert into [Order] (albumId, clientId, orderDate, sysStartTime, sysEndTime) values (20, 6 , '2019-10-21 11:41:52', '2019-10-21 11:41:52', '2019-10-26 05:03:45');
+insert into [OrderHistory] (albumId, clientId, orderDate, sysStartTime, sysEndTime) values (20, 6 , '2019-08-08 16:21:12', '2019-08-08 16:21:12', '2019-10-21 11:41:52');
+insert into [OrderHistory] (albumId, clientId, orderDate, sysStartTime, sysEndTime) values (20, 6 , '2019-10-21 11:41:52', '2019-10-21 11:41:52', '2019-10-26 05:03:45');
 #2
-insert into [Order] (albumId, clientId, orderDate, sysStartTime, sysEndTime) values (21, 27, '2019-05-17 01:49:10', '2019-05-17 01:49:10', '2019-05-18 01:49:10');
-insert into [Order] (albumId, clientId, orderDate, sysStartTime, sysEndTime) values (21, 27, '2019-05-18 01:49:10', '2019-05-18 01:49:10', '2019-05-26 06:03:52');
-insert into [Order] (albumId, clientId, orderDate, sysStartTime, sysEndTime) values (21, 27, '2019-05-26 06:03:52', '2019-05-26 06:03:52', '2019-06-14 00:27:24');
-insert into [Order] (albumId, clientId, orderDate, sysStartTime, sysEndTime) values (21, 27, '2019-06-14 00:27:24', '2019-06-14 00:27:24', '2019-06-15 00:26:23');
+insert into [OrderHistory] (albumId, clientId, orderDate, sysStartTime, sysEndTime) values (21, 27, '2019-05-17 01:49:10', '2019-05-17 01:49:10', '2019-05-18 01:49:10');
+insert into [OrderHistory] (albumId, clientId, orderDate, sysStartTime, sysEndTime) values (21, 27, '2019-05-18 01:49:10', '2019-05-18 01:49:10', '2019-05-26 06:03:52');
+insert into [OrderHistory] (albumId, clientId, orderDate, sysStartTime, sysEndTime) values (21, 27, '2019-05-26 06:03:52', '2019-05-26 06:03:52', '2019-06-14 00:27:24');
+insert into [OrderHistory] (albumId, clientId, orderDate, sysStartTime, sysEndTime) values (21, 27, '2019-06-14 00:27:24', '2019-06-14 00:27:24', '2019-06-15 00:26:23');
 #4
-insert into [Order] (albumId, clientId, orderDate, sysStartTime, sysEndTime) values (14, 16, '2019-02-18 07:42:56', '2019-02-18 07:42:56', '2019-02-20 07:42:56');
+insert into [OrderHistory] (albumId, clientId, orderDate, sysStartTime, sysEndTime) values (14, 16, '2019-02-18 07:42:56', '2019-02-18 07:42:56', '2019-02-20 07:42:56');
 #5
-insert into [Order] (albumId, clientId, orderDate, sysStartTime, sysEndTime) values (2 , 3 , '2018-02-19 00:27:24', '2018-02-19 00:27:24', '2018-03-10 10:53:45');
-insert into [Order] (albumId, clientId, orderDate, sysStartTime, sysEndTime) values (2 , 3 , '2018-03-10 10:53:45', '2018-03-10 10:53:45', '2018-06-14 10:58:31');
+insert into [OrderHistory] (albumId, clientId, orderDate, sysStartTime, sysEndTime) values (2 , 3 , '2018-02-19 00:27:24', '2018-02-19 00:27:24', '2018-03-10 10:53:45');
+insert into [OrderHistory] (albumId, clientId, orderDate, sysStartTime, sysEndTime) values (2 , 3 , '2018-03-10 10:53:45', '2018-03-10 10:53:45', '2018-06-14 10:58:31');
 #8
-insert into [Order] (albumId, clientId, orderDate, sysStartTime, sysEndTime) values (12, 30, '2018-07-15 00:57:50', '2018-07-15 00:57:50', '2018-07-16 23:50:56');
+insert into [OrderHistory] (albumId, clientId, orderDate, sysStartTime, sysEndTime) values (12, 30, '2018-07-15 00:57:50', '2018-07-15 00:57:50', '2018-07-16 23:50:56');
 #9
-insert into [Order] (albumId, clientId, orderDate, sysStartTime, sysEndTime) values (2 , 14, '2019-08-10 03:57:24', '2019-08-10 03:57:24', '2019-08-19 21:41:41');
+insert into [OrderHistory] (albumId, clientId, orderDate, sysStartTime, sysEndTime) values (2 , 14, '2019-08-10 03:57:24', '2019-08-10 03:57:24', '2019-08-19 21:41:41');
 #10
-insert into [Order] (albumId, clientId, orderDate, sysStartTime, sysEndTime) values (20, 24, '2018-01-15 23:58:59', '2018-01-15 23:58:59', '2019-06-15 19:58:07');
+insert into [OrderHistory] (albumId, clientId, orderDate, sysStartTime, sysEndTime) values (20, 24, '2018-01-15 23:58:59', '2018-01-15 23:58:59', '2019-06-15 19:58:07');
