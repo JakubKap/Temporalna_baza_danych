f716807dcf05c07d1929d917fd536710ee3f6e74
folusz-sebastian <folusz.sebastian@gmail.com>
2020-06-06 10:30:03 +0200

    Changed ArtistBand and added ArtistBandHistory

--- inserty/ArtistBandHistory.sql
+++ inserty/ArtistBandHistory.sql
@@ -1,10 +1,30 @@
-insert into ArtistBand (bandId, artistId, dateSinceArtistInBand, sysStartTime, sysEndTime) values (1, 5 , '1990-10-21 20:58:16', '1990-10-21 20:58:16', '9999-12-31 23:59:59.9999999');
-insert into ArtistBand (bandId, artistId, dateSinceArtistInBand, sysStartTime, sysEndTime) values (1, 11, '2005-06-12 08:56:50', '2005-06-12 08:56:50', '9999-12-31 23:59:59.9999999');
-insert into ArtistBand (bandId, artistId, dateSinceArtistInBand, sysStartTime, sysEndTime) values (1, 8 , '2011-12-01 16:10:19', '2011-12-01 16:10:19', '9999-12-31 23:59:59.9999999');
-insert into ArtistBand (bandId, artistId, dateSinceArtistInBand, sysStartTime, sysEndTime) values (1, 26, '1987-11-09 03:01:51', '1987-11-09 03:01:51', '9999-12-31 23:59:59.9999999');
-insert into ArtistBand (bandId, artistId, dateSinceArtistInBand, sysStartTime, sysEndTime) values (2, 7 , '1970-07-10 21:51:29', '1970-07-10 21:51:29', '9999-12-31 23:59:59.9999999');
-insert into ArtistBand (bandId, artistId, dateSinceArtistInBand, sysStartTime, sysEndTime) values (2, 1 , '2014-04-01 18:16:45', '2014-04-01 18:16:45', '9999-12-31 23:59:59.9999999');
-insert into ArtistBand (bandId, artistId, dateSinceArtistInBand, sysStartTime, sysEndTime) values (3, 4 , '2017-08-15 17:43:20', '2017-08-15 17:43:20', '9999-12-31 23:59:59.9999999');
-insert into ArtistBand (bandId, artistId, dateSinceArtistInBand, sysStartTime, sysEndTime) values (4, 13, '1985-01-06 21:18:14', '1985-01-06 21:18:14', '9999-12-31 23:59:59.9999999');
-insert into ArtistBand (bandId, artistId, dateSinceArtistInBand, sysStartTime, sysEndTime) values (4, 27, '2010-01-22 23:55:38', '2010-01-22 23:55:38', '9999-12-31 23:59:59.9999999');
-insert into ArtistBand (bandId, artistId, dateSinceArtistInBand, sysStartTime, sysEndTime) values (4, 20, '2010-06-08 09:19:01', '2010-06-08 09:19:01', '9999-12-31 23:59:59.9999999');
+
+# deleted
+insert into ArtistBandHistory (bandId, artistId, dateSinceArtistInBand, sysStartTime, sysEndTime) values (8, 16, '1996-03-29 03:49:56', '1996-03-29 03:49:56', '1996-05-29 11:17:31');
+insert into ArtistBandHistory (bandId, artistId, dateSinceArtistInBand, sysStartTime, sysEndTime) values (8, 17, '1996-03-22 16:08:50', '1996-03-22 16:08:50', '2000-03-22 09:08:23');
+insert into ArtistBandHistory (bandId, artistId, dateSinceArtistInBand, sysStartTime, sysEndTime) values (8, 18, '1996-10-08 17:59:18', '1996-10-08 17:59:18', '1996-11-10 14:35:04');
+insert into ArtistBandHistory (bandId, artistId, dateSinceArtistInBand, sysStartTime, sysEndTime) values (9, 19, '2011-03-24 14:25:39', '2011-03-24 14:25:39', '2012-01-12 12:49:58');
+insert into ArtistBandHistory (bandId, artistId, dateSinceArtistInBand, sysStartTime, sysEndTime) values (9, 20, '1987-05-20 03:29:22', '1987-05-20 03:29:22', '1987-05-20 05:37:57');
+insert into ArtistBandHistory (bandId, artistId, dateSinceArtistInBand, sysStartTime, sysEndTime) values (9, 21, '2019-01-20 14:03:28', '2019-01-20 14:03:28', '2020-01-18 03:57:31');
+
+# updated
+# 1
+insert into ArtistBandHistory (bandId, artistId, dateSinceArtistInBand, sysStartTime, sysEndTime) values (1, 5 , '1990-07-06 02:48:50', '1990-07-06 02:48:50', '1990-09-21 18:23:36');
+insert into ArtistBandHistory (bandId, artistId, dateSinceArtistInBand, sysStartTime, sysEndTime) values (1, 5 , '1990-09-21 18:23:36', '1990-09-21 18:23:36', '1990-10-18 23:10:07');
+insert into ArtistBandHistory (bandId, artistId, dateSinceArtistInBand, sysStartTime, sysEndTime) values (1, 5 , '1990-10-18 23:10:07', '1990-10-18 23:10:07', '1990-10-21 18:58:16');
+insert into ArtistBandHistory (bandId, artistId, dateSinceArtistInBand, sysStartTime, sysEndTime) values (1, 5 , '1990-10-21 18:58:16', '1990-10-21 18:58:16', '1990-10-21 20:58:16');
+# 2
+insert into ArtistBandHistory (bandId, artistId, dateSinceArtistInBand, sysStartTime, sysEndTime) values (1, 11, '2004-03-02 10:02:18', '2004-03-02 10:02:18', '2004-03-02 12:05:25');
+insert into ArtistBandHistory (bandId, artistId, dateSinceArtistInBand, sysStartTime, sysEndTime) values (1, 11, '2004-03-02 12:05:25', '2004-03-02 12:05:25', '2005-06-02 00:27:23');
+insert into ArtistBandHistory (bandId, artistId, dateSinceArtistInBand, sysStartTime, sysEndTime) values (1, 11, '2005-06-02 00:27:23', '2005-06-02 00:27:23', '2005-06-12 08:56:50');
+# 3
+insert into ArtistBandHistory (bandId, artistId, dateSinceArtistInBand, sysStartTime, sysEndTime) values (1, 8 , '2011-09-10 16:29:37', '2011-09-10 16:29:37', '2011-10-10 18:20:40');
+insert into ArtistBandHistory (bandId, artistId, dateSinceArtistInBand, sysStartTime, sysEndTime) values (1, 8 , '2011-10-10 18:20:40', '2011-10-10 18:20:40', '2011-10-10 20:37:50');
+insert into ArtistBandHistory (bandId, artistId, dateSinceArtistInBand, sysStartTime, sysEndTime) values (1, 8 , '2011-10-10 20:37:50', '2011-10-10 20:37:50', '2011-12-01 16:10:19');
+# 4
+insert into ArtistBandHistory (bandId, artistId, dateSinceArtistInBand, sysStartTime, sysEndTime) values (1, 26, '1987-07-21 04:13:26', '1987-07-21 04:13:26', '1987-07-24 23:17:43');
+insert into ArtistBandHistory (bandId, artistId, dateSinceArtistInBand, sysStartTime, sysEndTime) values (1, 26, '1987-07-24 23:17:43', '1987-07-24 23:17:43', '1987-11-09 03:01:51');
+# 5
+insert into ArtistBandHistory (bandId, artistId, dateSinceArtistInBand, sysStartTime, sysEndTime) values (2, 7 , '1970-07-01 22:58:32', '1970-07-01 22:58:32', '1970-07-10 21:51:29');
+# 10
+insert into ArtistBandHistory (bandId, artistId, dateSinceArtistInBand, sysStartTime, sysEndTime) values (4, 20, '2010-03-04 22:46:53', '2010-03-04 22:46:53', '2010-06-08 09:19:01');
